82f5af45115d04415d1e47ca7d52fe79cf68ca36
ron <ron.kirschler@gmail.com>
2014-11-09 14:56:26 +0100

    Initial commit

--- Logger.php
+++ Logger.php
@@ -7,8 +7,8 @@ interface Logger extends LoggerInterface {
 	/**
 	 * System is unusable.
 	 *
-	 * @param string $message
-	 * @param array $context
+	 * @param string $message The message MUST be a string or object implementing __toString().
+	 * @param array $context The context array can contain arbitrary data, the only assumption that can be made by implementors is that if an Exception instance is given to produce a stack trace, it MUST be in a key named "exception".
 	 * @return void
 	 */
 	public function emergency($message, array $context = array());
@@ -19,8 +19,8 @@ interface Logger extends LoggerInterface {
 	 * Example: Entire website down, database unavailable, etc. This should
 	 * trigger the SMS alerts and wake you up.
 	 *
-	 * @param string $message
-	 * @param array $context
+	 * @param string $message The message MUST be a string or object implementing __toString().
+	 * @param array $context The context array can contain arbitrary data, the only assumption that can be made by implementors is that if an Exception instance is given to produce a stack trace, it MUST be in a key named "exception".
 	 * @return void
 	 */
 	public function alert($message, array $context = array());
@@ -30,8 +30,8 @@ interface Logger extends LoggerInterface {
 	 *
 	 * Example: Application component unavailable, unexpected exception.
 	 *
-	 * @param string $message
-	 * @param array $context
+	 * @param string $message The message MUST be a string or object implementing __toString().
+	 * @param array $context The context array can contain arbitrary data, the only assumption that can be made by implementors is that if an Exception instance is given to produce a stack trace, it MUST be in a key named "exception".
 	 * @return void
 	 */
 	public function critical($message, array $context = array());
@@ -40,8 +40,8 @@ interface Logger extends LoggerInterface {
 	 * Runtime errors that do not require immediate action but should typically
 	 * be logged and monitored.
 	 *
-	 * @param string $message
-	 * @param array $context
+	 * @param string $message The message MUST be a string or object implementing __toString().
+	 * @param array $context The context array can contain arbitrary data, the only assumption that can be made by implementors is that if an Exception instance is given to produce a stack trace, it MUST be in a key named "exception".
 	 * @return void
 	 */
 	public function error($message, array $context = array());
@@ -52,8 +52,8 @@ interface Logger extends LoggerInterface {
 	 * Example: Use of deprecated APIs, poor use of an API, undesirable things
 	 * that are not necessarily wrong.
 	 *
-	 * @param string $message
-	 * @param array $context
+	 * @param string $message The message MUST be a string or object implementing __toString().
+	 * @param array $context The context array can contain arbitrary data, the only assumption that can be made by implementors is that if an Exception instance is given to produce a stack trace, it MUST be in a key named "exception".
 	 * @return void
 	 */
 	public function warning($message, array $context = array());
@@ -61,8 +61,8 @@ interface Logger extends LoggerInterface {
 	/**
 	 * Normal but significant events.
 	 *
-	 * @param string $message
-	 * @param array $context
+	 * @param string $message The message MUST be a string or object implementing __toString().
+	 * @param array $context The context array can contain arbitrary data, the only assumption that can be made by implementors is that if an Exception instance is given to produce a stack trace, it MUST be in a key named "exception".
 	 * @return void
 	 */
 	public function notice($message, array $context = array());
@@ -72,8 +72,8 @@ interface Logger extends LoggerInterface {
 	 *
 	 * Example: User logs in, SQL logs.
 	 *
-	 * @param string $message
-	 * @param array $context
+	 * @param string $message The message MUST be a string or object implementing __toString().
+	 * @param array $context The context array can contain arbitrary data, the only assumption that can be made by implementors is that if an Exception instance is given to produce a stack trace, it MUST be in a key named "exception".
 	 * @return void
 	 */
 	public function info($message, array $context = array());
@@ -81,8 +81,8 @@ interface Logger extends LoggerInterface {
 	/**
 	 * Detailed debug information.
 	 *
-	 * @param string $message
-	 * @param array $context
+	 * @param string $message The message MUST be a string or object implementing __toString().
+	 * @param array $context The context array can contain arbitrary data, the only assumption that can be made by implementors is that if an Exception instance is given to produce a stack trace, it MUST be in a key named "exception".
 	 * @return void
 	 */
 	public function debug($message, array $context = array());
@@ -90,9 +90,9 @@ interface Logger extends LoggerInterface {
 	/**
 	 * Logs with an arbitrary level.
 	 *
-	 * @param mixed $level
-	 * @param string $message
-	 * @param array $context
+	 * @param string $level See Psr\Log\LogLevel::*
+	 * @param string $message The message MUST be a string or object implementing __toString().
+	 * @param array $context The context array can contain arbitrary data, the only assumption that can be made by implementors is that if an Exception instance is given to produce a stack trace, it MUST be in a key named "exception".
 	 * @return void
 	 */
 	public function log($level, $message, array $context = array());
